d871649fd2253dfd7d2ee8cf40d7a9d48473d8ab
Baris <karalarbaris@gmail.com>
2021-04-24 09:41:45 +0200

    235. TableView Delegate Methods and Accessories

---
+++ Todoey/K.swift
@@ -0,0 +1,15 @@
+//
+//  K.swift
+//  Todoey
+//
+//  Created by Baris Karalar on 23.04.2021.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let toDoListCellIdentifier = "ToDoItemCell"
+    
+    
+}
